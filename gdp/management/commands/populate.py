30c75a0663bf9c60c2f264dede53651150e2b57e
Simone Dalla <simodalla@gmail.com>
2022-08-22 12:52:18 +0200

    fine video #11

--- gdp/management/commands/populate.py
+++ gdp/management/commands/populate.py
@@ -1,8 +1,33 @@
+import itertools
+import json
+
+from django.conf import settings
 from django.core.management.base import BaseCommand
+
+from ...models import GDP
 
 
 class Command(BaseCommand):
-    help = 'Load Courses and Modules'
+    help = "Load Courses and Modules"
 
     def handle(self, *args, **kwargs):
-        raise NotImplementedError
+        if GDP.objects.count():
+            return 10
+
+        datafile = settings.BASE_DIR / "data" / "gdp.json"
+        with open(datafile, "r") as f:
+            data = json.load(f)
+
+        data = itertools.dropwhile(lambda x: x["Country Name"] != "Afghanistan", data)
+
+        gdps = []
+        for d in data:
+            gdps.append(
+                GDP(
+                    country=d["Country Name"],
+                    country_code=d["Country Code"],
+                    year=d["Year"],
+                    gdp=d["Value"],
+                )
+            )
+        GDP.objects.bulk_create(gdps)
--- gdp/views.py
+++ gdp/views.py
@@ -1,5 +1,54 @@
+import math
+
+from django.db.models import Max, Min
 from django.shortcuts import render
 
-# Create your views here.
+from bokeh.embed import components
+from bokeh.models import ColumnDataSource, HoverTool, NumeralTickFormatter
+from bokeh.plotting import figure
+
+from .models import GDP
+
+
 def index(request):
-    return render(request, 'index.html')
+    # define which year we want the data from
+    max_year = GDP.objects.aggregate(max_yr=Max("year"))["max_yr"]
+    min_year = GDP.objects.aggregate(min_yr=Min("year"))["min_yr"]
+    year = request.GET.get("year", max_year)
+
+    # define number of countires to fetch
+    count = int(request.GET.get("count", 10))
+
+    gdps = GDP.objects.filter(year=year).order_by("gdp").reverse()[:count]
+
+    country_names = [d.country for d in gdps]
+    country_gdps = [d.gdp for d in gdps]
+
+    cds = ColumnDataSource(
+        data=dict(
+            country_names=country_names,
+            country_gdps=country_gdps,
+        )
+    )
+
+    fig = figure(x_range=country_names, height=500, title=f"Top {count} GDPs ({year})")
+    fig.title.align = "center"
+    fig.title.text_font_size = "1.5em"
+    fig.yaxis[0].formatter = NumeralTickFormatter(format="$0.0a")
+    fig.xaxis.major_label_orientation = math.pi / 4
+
+    fig.vbar(source=cds, x="country_names", top="country_gdps", width=0.8)
+    tooltips = [("Country", "@country_names"), ("GDP", "@country_gdps{,}")]
+    fig.add_tools(HoverTool(tooltips=tooltips))
+
+    script, div = components(fig)
+    context = {
+        "script": script,
+        "div": div,
+        "years": range(min_year, max_year + 1),
+        "count": count,
+        "year_selected": year,
+    }
+
+    template = "partials/gdp-bar.html" if request.htmx else "index.html"
+    return render(request, template, context)
